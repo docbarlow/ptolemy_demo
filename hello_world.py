d2a90159d97847e8502528f7ea1f44bd0283447e
docbarlow <164091902+docbarlow@users.noreply.github.com>
2024-03-19 16:43:03 -0500

    Initial Commit
    Commit of python program that uses Tensor Flow to train a model capable of recognizing handwritten digits contained in the mnist dataset. Contains the data file you will need, the environment file you will need, and a "how to" document for using all of this code on the Ptolemy cluster at Mississippi State University.

---
+++ hello_world.py
@@ -0,0 +1,100 @@
+# For math
+import numpy as np
+
+# For data frames
+import pandas as pd
+
+# For generating random numbers (already bundled with Python language)
+import random
+
+# Our AI package
+import tensorflow as tf
+
+import os
+
+from tensorflow.keras.models import Sequential, model_from_json
+from tensorflow.keras.layers import Flatten, Conv2D, Dense, MaxPooling2D
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+
+# see if we have multiple gpus
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+# this is convenient - exit the script immediatley if it isn't using tensor flow
+# built with cuda
+if not tf.test.is_built_with_cuda():
+    print("Not built with cuda")
+    exit(1)
+
+# set path to mnist file
+file_path = os.getenv('DATA_FILE_PATH') + "/mnist.npz"
+
+# load the mnist data
+(x_train, y_train), (x_test, y_test) = mnist.load_data(file_path)
+
+# reshape to set up grayscale color channel for each 28x28 image
+x_train = x_train.reshape((x_train.shape[0], 28, 28, 1))
+
+# normalize color between 0 and 1
+x_train = x_train.astype ('float32') / 255.0
+
+# create label for it
+y_train = to_categorical (y_train)
+
+# setup the model
+
+model = Sequential([
+    # Input shape is 28x28x1 like our images
+    # There will be 32 convolutions (little parts of the image) that are 3x3
+    # The activation function will be RELU; we talked about it in class
+    Conv2D(32, (3, 3), activation='relu', strides=1, input_shape=(28, 28,1)),
+    # The next layer should pool together all the stuff we learned in the previous
+    # layer - (TODO: explain pooling layers and why they are helpful)
+    MaxPooling2D((2, 2)),
+    # Get a flat tensor - a continuous list of values
+    Flatten(),
+    # TODO: explain this one
+    Dense(100, activation='relu'),
+    # This is our last or "output layer". Notice it has 10 values like our labels (0-9).
+    # Softmax gives us a probability distribution so we can see which digit is most likely
+    # 
+    Dense(10, activation='softmax')
+])
+
+optimizer = SGD(learning_rate=0.01, momentum=0.9)
+
+# compile the model
+
+model.compile(
+    optimizer=optimizer,
+    loss='categorical_crossentropy',
+    metrics=[ 'accuracy' ]
+)
+
+# fit the model
+history = model.fit(x_train, y_train, epochs=10, batch_size=32)
+
+# serialize model to JSON
+model_json = model.to_json()
+
+with open("model1.json", "w") as json_file:
+    json_file.write(model_json)
+
+# serialize weights to HDF5
+model.save_weights("model1.h5")
+
+print("Saved model to disk")
+
+# Reshape all the test data
+x_test = x_test.reshape((x_test.shape[0], 28, 28, 1))
+x_test = x_test.astype ('float32') / 255.0
+y_test = to_categorical(y_test)
+
+# Now let's test the model
+score = model.evaluate(x_test, y_test, verbose=1)
+
+print("%s: %.2f%%" % (model.metrics_names[1], score[1]*100))
+
+
+
---
+++ slurm.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+## This line requests that the job be allocated one node
+#SBATCH -N 1
+
+## This line requests one task for the job
+#SBATCH -n 1
+
+## This specifies the amount of memory required for the job. 
+## Here, it's set to 10 Gigabytes. This memory is allocated per node.
+#SBATCH --mem=10G
+
+## This sets the partition (or queue) to gpu-a100
+## Partitions are defined by the system administrators and 
+## determine the set of nodes the job can run on. 
+#SBATCH -p gpu-a100
+
+## This specifies the account for job charging or accounting. 
+## Here, it's set to an account named 'jhb11'.
+#SBATCH -A test
+
+## This sets the time limit for the job. 
+## Here, the job can run for up to 1 hour. 
+## If the job exceeds this limit, SLURM will terminate it.
+#SBATCH -t 1:00:00
+
+## This requests a specific generic resource (GRES), in this case, 
+## a GPU. Specifically, it requests one Nvidia A100 GPU with 10 GB of memory.
+#SBATCH --gres=gpu:a100_1g.10gb:1
+
+## This sets the name of the job to "jhb11-GPU-mnist". 
+## This name will appear in the queue and in various SLURM reports, 
+## helping you to identify your job.
+#SBATCH --job-name "jhb11-GPU-mnist"
+
+## This specifies that all the output from the job (stdout) will 
+## be written to the file output.out. This file will be created in the 
+## directory from which the SLURM script is submitted.
+#SBATCH --output=output.out
+
+## each of these lines uses the "module load" command
+## to load the modules our python script will need to use
+ml cuda
+ml python/3.9.15
+ml miniconda3/4.10.3
+
+## change directories to your user scratch location where your 
+## script is
+cd /scratch/ptolemy/users/jhb11/hello_world
+
+## now create your conda environment - doesn't work through slurm??
+## conda env create --file ./environment.yml --name jhb11-GPU-mnist --force
+
+## activate conda environment
+source activate jhb11-GPU-mnist
+
+## Set a path you can use inside your python script to find your data files
+export DATA_FILE_PATH="/scratch/ptolemy/users/jhb11/hello_world"
+
+## change directories to your user scratch location where your 
+## script is
+cd /scratch/ptolemy/users/jhb11/hello_world
+
+## run the script
+python hello_world.py
